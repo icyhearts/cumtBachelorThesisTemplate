eb60d0fed3ed52c835e7435081632586bba8d378
ice <icyheartshk@gmail.com>
2017-06-23 16:15:53 +0800

    add pictures recursively

--- gitadd.sh
+++ gitadd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git add *.tex *.sh thesisRefs.bib cumtunsrtnat.bst cumtBachelorThesisTemplate.pdf gaussianAndDog.PNG useful/
+git add *.tex *.sh thesisRefs.bib cumtunsrtnat.bst cumtBachelorThesisTemplate.pdf gaussianAndDog.PNG useful/compareEdgeResponse/
